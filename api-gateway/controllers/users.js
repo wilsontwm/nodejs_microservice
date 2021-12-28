9af02410b433aecb852f3ccd819ba54de3b2ad55
Wilson <wilson.tan@revenuemonster.my>
2021-12-28 23:17:40 +0800

    added activate and resend activate code

--- api-gateway/controllers/users.js
+++ api-gateway/controllers/users.js
@@ -47,4 +47,91 @@ module.exports =  class UserController extends Controller {
             });
         }
     }
+
+    user_login = async(req, res, next) => {
+        try {
+            let rules = {
+                email: 'required|email',
+                password: 'required'
+            };
+              
+            let validation = new Validator(req.body, rules);
+            
+            if(!validation.passes()) {
+                return res.status(412).json(validation.errors);
+            }
+            
+            // Login user
+            var loginUserRequest = new userMessage.LoginUserRequest();
+            loginUserRequest.setEmail(req.body.email);
+            loginUserRequest.setPassword(req.body.password);
+            
+            const response = await this.services.userService.loginUser(loginUserRequest);
+            
+            return res.status(200).json(response.toObject())
+    
+        } catch (e){
+            return res.status(500).json({
+                code: e.code,
+                error: e.details,
+                debug: e.message
+            });
+        }
+    }
+
+    user_resend_activation = async(req, res, next) => {
+        try {
+            let rules = {
+                email: 'required|email'
+            };
+              
+            let validation = new Validator(req.body, rules);
+            
+            if(!validation.passes()) {
+                return res.status(412).json(validation.errors);
+            }
+            
+            var resendActivationCodeRequest = new userMessage.ResendActivationCodeRequest();
+            resendActivationCodeRequest.setEmail(req.body.email);
+            
+            const response = await this.services.userService.resendActivationCode(resendActivationCodeRequest);
+            
+            return res.status(200).json(response.toObject())
+    
+        } catch (e){
+            return res.status(500).json({
+                code: e.code,
+                error: e.details,
+                debug: e.message
+            });
+        }
+    }
+
+    user_activate = async(req, res, next) => {
+        try {
+            let rules = {
+                code: 'required'
+            };
+
+            let validation = new Validator(req.params, rules);
+            
+            if(!validation.passes()) {
+                return res.status(412).json(validation.errors);
+            }
+            
+            var activateUserRequest = new userMessage.ActivateUserRequest();
+            activateUserRequest.setCode(req.params.code);
+            
+            const response = await this.services.userService.activateUser(activateUserRequest);
+            
+            return res.status(200).json(response.toObject())
+    
+        } catch (e){
+            return res.status(500).json({
+                code: e.code,
+                error: e.details,
+                debug: e.message
+            });
+        }
+    }
 }
--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -6,9 +6,9 @@ const checkAuthentication = require('../middleware/checkAuthentication');
 const controllers = getControllers();
 
 router.post('/register', controllers.userController.user_register);
-// router.post('/login', UserController.users_login);
-// router.post('/activate/resend', UserController.users_resend_activation);
-// router.get('/activate/:code', UserController.users_activate);
+router.post('/login', controllers.userController.user_login);
+router.post('/activate/resend', controllers.userController.user_resend_activation);
+router.get('/activate/:code', controllers.userController.user_activate);
 // router.post('/password/forget', UserController.users_password_forget);
 // router.post('/password/reset/:token', UserController.users_password_reset);
 // router.post('/profile', checkAuthentication, UserController.users_profile_update);
